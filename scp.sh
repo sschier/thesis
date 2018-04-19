b3c7ed57c9baba516707f06ba0cb852594d5459f
Sheena Schier <sschier@ucsc.edu>
2018-04-19 15:13:54 -0500

    update scp script

---
+++ scp.sh
@@ -0,0 +1,56 @@
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/histfitter/exclusion_contour_slepton.pdf results/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/histfitter/exclusion_contour_higgsino.pdf results/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/histfitter/pull_plot_summary_yields.pdf results/.
+#
+#
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/bkgModellingSysts/alphaVars* systematics/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/bkgModellingSysts/PDFVars* systematics/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/bkgModellingSysts/scaleVars* systematics/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/bkgModellingSysts/VVcomp* systematics/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/bkgModellingSysts/ttbarComp*_v2.pdf systematics/.
+#
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/backgrounds/dataCR_mll*_pre.pdf backgrounds/.
+
+# add CRtop,CRtau, and VRVV to backgrounds
+
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/backgrounds/fakefactor_schematic.tex fakes/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/* fakes/FF_electron/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_muon/* fakes/FF_muon/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/AID_deco* fakes/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/ID_CR_M* fakes/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/AID_CR_M* fakes/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/ID_SR_IDelPt.pdf fakes/.
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/FF_electron/AID_SR_AntiIDelPt.pdf fakes/.
+
+#scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/fakeLeptonComposition/* fakes/fakeLeptonComposition/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_SR/lep2Pt.pdf fakes/antiIDStudies/.
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_SR_lep2Pt.pdf
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_VeryLoose_FailSignal/lep2Pt.pdf fakes/antiIDStudies/.
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_VeryLoose_FailSignal_lep2Pt.pdf
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_VeryLooseBL_FailSignal/lep2Pt.pdf fakes/antiIDStudies/. 
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_VeryLooseBL_FailSignal_lep2Pt.pdf
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_LooseBL_FailSignal/lep2Pt.pdf fakes/antiIDStudies/. 
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_LooseBL_FailSignal_lep2Pt.pdf
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_Medium_FailSignal/lep2Pt.pdf fakes/antiIDStudies/. 
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_Medium_FailSignal_lep2Pt.pdf
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/antiIDStudies/AllMC_ee_LooseBL_D0SigGt1.5_OR_Medium/lep2Pt.pdf fakes/antiIDStudies/. 
+mv fakes/antiIDStudies/lep2Pt.pdf fakes/antiIDStudies/AllMC_ee_LooseBL_D0SigGt1.5_OR_Medium_lep2Pt.pdf
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/sleptons/slepton_higgsino_13TeV_xsect.pdf signal_samples/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/mll_theory.pdf signal_samples/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/postFitPlots/Higgsino_bkg_VRDF_iMT2f_VRDF_iMT2f_lep2Pt.pdf results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/postFitPlots/Higgsino_bkg_VR_VV_VR_VV_mt2leplsp_100.pdf results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/postFitPlots/Higgsino_bkg_VR_SS_AF_VR_SS_AF_lep2Pt.pdf results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/postFitPlots/Higgsino_bkg_VR_SS_AF_VR_SS_AF_mll.pdf results/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/postFitPlots/Higgsino_bkg_SRSF* results/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/tables/results/Merged_SimpleDiscoveryUpperLimits_unrounded.tex results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/tables/results/MyYieldsTable_exclSRs.tex results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/histpull_HiggsinoFit_doCRonly_VRs.pdf results/.
+
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/paper/figures/results/histpull_HiggsinoFit_doCRplusSRMLLandMT2_exclSR.pdf results/.
+scp sschier@atlas02.ucsc.edu:/export/share/data/sschier/Higgsino/Note/Compressed/figures/histfitter/fitParams.tex results/.
